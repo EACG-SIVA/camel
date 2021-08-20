4cec25f91b3ce6cdeee66d3bf802b6aa2e13eb43
Sivareddy Lebaka <sivareddy>
2021-08-20 22:12:37 +0530

    SCC-3 : Modified CompositeGraphIntegrationTest

--- components/camel-salesforce/camel-salesforce-component/src/test/java/org/apache/camel/component/salesforce/CompositeGraphIntegrationTest.java
+++ components/camel-salesforce/camel-salesforce-component/src/test/java/org/apache/camel/component/salesforce/CompositeGraphIntegrationTest.java
@@ -24,56 +24,43 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.thoughtworks.xstream.annotations.XStreamImplicit;
-import org.apache.camel.CamelExecutionException;
-import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.salesforce.CompositeApiIntegrationTest.Accounts;
-import org.apache.camel.component.salesforce.api.dto.AbstractQueryRecordsBase;
-import org.apache.camel.component.salesforce.api.dto.CreateSObjectResult;
-import org.apache.camel.component.salesforce.api.dto.composite.SObjectComposite;
-import org.apache.camel.component.salesforce.api.dto.composite.SObjectComposite.Method;
 import org.apache.camel.component.salesforce.api.dto.composite.SObjectCompositeResponse;
 import org.apache.camel.component.salesforce.api.dto.composite.SObjectCompositeResult;
-import org.apache.camel.component.salesforce.api.utils.Version;
-import org.apache.camel.component.salesforce.dto.generated.Account;
-import org.apache.camel.component.salesforce.dto.generated.Line_Item__c;
 import org.apache.camel.test.junit5.params.Parameter;
 import org.apache.camel.test.junit5.params.Parameterized;
 import org.apache.camel.test.junit5.params.Parameters;
 import org.apache.camel.test.junit5.params.Test;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 
 @Parameterized
 public class CompositeGraphIntegrationTest extends AbstractSalesforceTestBase {
 
-	/*public static class Accounts extends AbstractQueryRecordsBase {
+    /*public static class Accounts extends AbstractQueryRecordsBase {
         @XStreamImplicit
         private List<Account> records;
-
+    
         public List<Account> getRecords() {
             return records;
         }
-
+    
         public void setRecords(final List<Account> records) {
             this.records = records;
         }
-
-    }
+    
+    }*/
 
     private static final Set<String> VERSIONS = new HashSet<>(Arrays.asList("38.0", "50.0"));
-
+    /*
     @Parameter
     private String format;
-
+    */
     @Parameter(1)
     private String version;
-
+    /*
     private String accountId;
-
+    
     private String compositeUri;
-
+    
     @AfterEach
     public void removeRecords() {
         try {
@@ -81,98 +68,98 @@ public class CompositeGraphIntegrationTest extends AbstractSalesforceTestBase {
         } catch (final CamelExecutionException ignored) {
             // other tests run in parallel could have deleted the Account
         }
-
+    
         template.request("direct:deleteBatchAccounts", null);
     }
-
+    
     @BeforeEach
     public void setupRecords() {
         compositeUri = "salesforce:composite?format=" + format;
-
+    
         final Account account = new Account();
         account.setName("Composite API Batch");
-
+    
         final CreateSObjectResult result = template.requestBody("salesforce:createSObject", account, CreateSObjectResult.class);
-
+    
         accountId = result.getId();
     }
-
+    
     @Test
     public void shouldSubmitBatchUsingCompositeApi() {
         final SObjectComposite composite = new SObjectComposite(version, true);
-
+    
         final Account updates = new Account();
         updates.setName("NewName");
         composite.addUpdate("Account", accountId, updates, "UpdateExistingAccountReferenceId");
-
+    
         final Account newAccount = new Account();
         newAccount.setName("Account created from Composite batch API");
         composite.addCreate(newAccount, "CreateAccountReferenceId");
-
+    
         composite.addGet("Account", accountId, "GetAccountReferenceId", "Name", "BillingPostalCode");
-
+    
         composite.addDelete("Account", accountId, "DeleteAccountReferenceId");
-
+    
         testComposite(composite);
     }
-
+    
     @Test
     public void shouldSupportGenericCompositeRequests() {
         final SObjectComposite composite = new SObjectComposite(version, true);
-
+    
         composite.addGeneric(Method.GET, "/sobjects/Account/" + accountId, "GetExistingAccountReferenceId");
-
+    
         testComposite(composite);
     }
-
+    
     @Test
     public void shouldSupportObjectCreation() {
         final SObjectComposite composite = new SObjectComposite(version, true);
-
+    
         final Account newAccount = new Account();
         newAccount.setName("Account created from Composite batch API");
         composite.addCreate(newAccount, "CreateAccountReferenceId");
-
+    
         final SObjectCompositeResponse response = testComposite(composite);
-
+    
         assertResponseContains(response, "id");
     }
-
+    
     @Test
     public void shouldSupportObjectDeletion() {
         final SObjectComposite composite = new SObjectComposite(version, true);
         composite.addDelete("Account", accountId, "DeleteAccountReferenceId");
-
+    
         testComposite(composite);
     }
-
+    
     @Test
     public void shouldSupportObjectRetrieval() {
         final SObjectComposite composite = new SObjectComposite(version, true);
-
+    
         composite.addGet("Account", accountId, "GetExistingAccountReferenceId", "Name");
-
+    
         final SObjectCompositeResponse response = testComposite(composite);
-
+    
         assertResponseContains(response, "Name");
     }
-
+    
     @Test
     public void shouldSupportObjectUpdates() {
         final SObjectComposite composite = new SObjectComposite(version, true);
-
+    
         final Account updates = new Account();
         updates.setName("NewName");
         updates.setAccountNumber("AC12345");
         composite.addUpdate("Account", accountId, updates, "UpdateAccountReferenceId");
-
+    
         testComposite(composite);
     }
-
+    
     @Test
     public void shouldSupportObjectUpserts() {
         final SObjectComposite composite = new SObjectComposite(version, true);
-
+    
         final Line_Item__c li = new Line_Item__c();
         composite.addUpsertByExternalId("Line_Item__c", "Name", "AC12345", li,
                 "UpsertLineItemReferenceId");
@@ -196,49 +183,49 @@ public class CompositeGraphIntegrationTest extends AbstractSalesforceTestBase {
                 response, SObjectCompositeResponse.class);
         assertResponseContains(sObjectCompositeResponse, "done");
     }
-/*
+    /*
     @Test
     public void shouldSupportQuery() {
         final SObjectComposite composite = new SObjectComposite(version, true);
         composite.addQuery("SELECT Id, Name FROM Account", "SelectQueryReferenceId");
-
+    
         final SObjectCompositeResponse response = testComposite(composite);
-
+    
         assertResponseContains(response, "totalSize");
     }
-
+    
     @Test
     public void shouldSupportQueryAll() {
         final SObjectComposite composite = new SObjectComposite(version, true);
         composite.addQueryAll("SELECT Id, Name FROM Account", "SelectQueryReferenceId");
-
+    
         final SObjectCompositeResponse response = testComposite(composite);
-
+    
         assertResponseContains(response, "totalSize");
     }
-
+    
     @Test
     public void shouldSupportRelatedObjectRetrieval() {
         if (Version.create(version).compareTo(Version.create("36.0")) < 0) {
             return;
         }
-
+    
         final SObjectComposite composite = new SObjectComposite("36.0", true);
         composite.addGetRelated("Account", accountId, "CreatedBy", "GetRelatedAccountReferenceId");
-
+    
         final SObjectCompositeResponse response = testComposite(composite);
-
+    
         assertResponseContains(response, "Username");
     }
-
+    
     SObjectCompositeResponse testComposite(final SObjectComposite batch) {
         final SObjectCompositeResponse response = template.requestBody(compositeUri, batch, SObjectCompositeResponse.class);
-
+    
         Assertions.assertThat(response).as("Response should be provided").isNotNull();
-
+    
         Assertions.assertThat(response.getCompositeResponse()).as("Received errors in: " + response)
                 .allMatch(val -> val.getHttpStatusCode() >= 200 && val.getHttpStatusCode() <= 299);
-
+    
         return response;
     }*/
 
@@ -250,7 +237,7 @@ public class CompositeGraphIntegrationTest extends AbstractSalesforceTestBase {
 
         return response;
     }
-/*
+    /*
     @Override
     protected RouteBuilder doCreateRouteBuilder() throws Exception {
         return new RouteBuilder() {
@@ -264,11 +251,11 @@ public class CompositeGraphIntegrationTest extends AbstractSalesforceTestBase {
             }
         };
     }
-
+    
     @Override
     protected String salesforceApiVersionToUse() {
         return version;
-    }
+    }*/
 
     @Parameters(name = "format = {0}, version = {1}")
     public static Iterable<Object[]> formats() {
@@ -291,5 +278,5 @@ public class CompositeGraphIntegrationTest extends AbstractSalesforceTestBase {
         final Map<String, ?> body = (Map<String, ?>) firstCompositeResponseBody;
         Assertions.assertThat(body).containsKey(key);
         Assertions.assertThat(body.get(key)).isNotNull();
-    }*/
+    }
 }
